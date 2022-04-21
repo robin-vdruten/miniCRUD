db6f9accf5894ab99f731b0a371d44be255ce357
robin-vdruten <1196364@student.roc-nijmegen.nl>
2022-04-21 13:08:43 +0200

    liveSearch

--- Includes/searchBar.php
+++ Includes/searchBar.php
@@ -7,7 +7,10 @@ echo '<form action="menu.php" id="search-form" method="GET">
         name="search"
         id="search-box"
       />
-      <div id="show-up"></div>
+      <div class="show">
+      <div id="show-up">
+      </div>  
+      </div>
       </div>
       <label for="search-box" class="fas fa-search"></label>
       <i class="fas fa-times" id="close"></i>
--- PHP/search.php
+++ PHP/search.php
@@ -7,18 +7,26 @@ function search($text)
     $text = htmlspecialchars($text);
 
     $stmt = $conn->prepare(
-        "SELECT titel FROM menu WHERE titel LIKE concat('%', :titel, '%')"
+        "SELECT * FROM menu WHERE titel LIKE concat('%',:titel, '%')"
     );
 
     $stmt->execute(['titel' => $text]);
-
     while ($names = $stmt->fetch(PDO::FETCH_ASSOC)) {
         echo '<a href="menu.php?search=' . $names['titel'] . '"';
-        echo '>' . $names['titel'] . '';
+        echo '<div class="box">';
+        echo '<div class="image">';
+        echo '<img src="Images/' . $names['foto'] . '"/>';
+        echo '</div>';
+        echo '<div class="content">';
+        echo '<h3>' . $names['titel'] . '</h3>';
+        echo '<p>';
+        echo '';
+        echo '</p>';
+        echo '<span class="price">$' . $names['prijs'] . '</span>';
+        echo '</div>';
+        echo '</div>';
         echo '</a>';
     }
 }
 
-search($_GET['txt']);
-?>
-
+search($_GET['search']); ?>
