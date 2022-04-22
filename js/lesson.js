c942b9cb293332bdef7cc58df76bb421e2f7e8cc
robin-vdruten <1196364@student.roc-nijmegen.nl>
2022-04-22 12:05:03 +0200

    changes before leon

--- js/lesson.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector(".container");
-
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    container.innerHTML = "<img src=" + data.sprites.front_default + ">";
-  })
-  .catch((e) => console.log(e));
